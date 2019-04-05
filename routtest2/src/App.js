abc6590486cae5151abf0df9a2a7589df22ba4e1
Kristiyan Gochev <kristiyan.gochev@playboxtechnology.com>
2019-04-05 16:27:28 +0300

    Creating some data in store.json and initialize componentDidMount method

--- routtest2/src/App.js
+++ routtest2/src/App.js
@@ -3,6 +3,11 @@ export default class extends Component{
   state ={
     writers:[]
   }
+  componentDidMount(){
+    fetch('http://localhost:4000/writers')
+    .then(res => res.json())
+    .then(writers =>this.setState({writers}))
+  }
 render(){
   return<div>
      123</div>
