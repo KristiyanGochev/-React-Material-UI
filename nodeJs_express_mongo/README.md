badd5b66a8429b04ce0a8b4adf386b4b0cf9abb2
Kristiyan Gochev <kristiyan.gochev@playboxtechnology.com>
2019-04-15 16:36:38 +0300

    Starting course with Mosh

--- nodeJs_express_mongo/README.md
+++ nodeJs_express_mongo/README.md
@@ -1,3 +1,5 @@
 ### Source video: https://www.youtube.com/watch?v=pKd0Rpw7O48
     - Set an process evn command : $env: PORT= 5000
     - install nodemon by : npm i -g nodemon
+
+https://codewithmosh.com/courses/293204/lectures/4509035
--- nodeJs_express_mongo/app.js
+++ nodeJs_express_mongo/app.js
@@ -7,10 +7,25 @@ app.get('/', (req, res)=> {
     res.send('Hello World!');
 
 });
-app.get('/test', (req, res)=> {
-    res.send('TestPage');
 
+app.get('/api/courses', (req, res)=> {
+    res.send([1,2,3]);
 });
+
+// /api/courses/1
+app.get('/api/courses/:id',(req,res)=>{
+res.send(req.params.id)
+})
+
+// /api/posts/2018/11
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send(req.params)
+})
+// /api/posts/2018/11?SortBy=Name
+app.get('/api/posts/:year/:month/:day',(req,res)=>{
+    res.send(req.query)
+})
+
 app.listen(port, () => {
     console.log(`Server is up and running on port numner ${port}`);
 }); 
